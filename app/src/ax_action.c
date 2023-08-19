23cedbe51e7ecea80bb11893ce421f6127156b66
lazilyBird <brokenmoon@126.com>
2023-08-19 17:49:01 +0800

    add ax files

---
+++ app/src/ax_action.c
@@ -0,0 +1,20 @@
+//
+//
+//
+
+#include "scrcpy.h"
+#include "ax_action.h"
+#include "util/log.h"
+
+int start_ax_thread()
+{
+    LOGI("starting ax thread");
+
+    return SCRCPY_EXIT_SUCCESS;
+}
+int stop_ax_thread()
+{
+    LOGI("stopping ax thread");
+
+    return SCRCPY_EXIT_SUCCESS;
+}
---
+++ app/src/ax_action.h
@@ -0,0 +1,11 @@
+//
+//
+//
+
+#ifndef AX_ACTION_H
+#define AX_ACTION_H
+
+int start_ax_thread();
+int stop_ax_thread();
+
+#endif // end AX_ACTION_H
